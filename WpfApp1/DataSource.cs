5840ec80c50657f86eafc253c1c03a1c915ca179
Chingiz Akhmedov <c.akhmedov@itstep.edu.az>
2021-07-13 21:58:46 +0400

    Add project files.

---
+++ WpfApp1/DataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class DataSource
+    {
+        private IEnumerable<Student> students;
+        public DataSource()
+        {
+            students = new List<Student> { new Student("Dmitriy",20,11.9f),
+                new Student("Javid", 18, 11.999f) { IsStudy = true},
+                new Student("Kanan", 19, 11f) { IsStudy = true}
+            };
+        }
+
+        public IEnumerable<Student> Students => students;
+
+    }
+}
---
+++ WpfApp1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Student
+    {
+        string fullName;
+        int age;
+        float score;
+       
+        public Student(string fullName, int age, float score)
+        {
+            this.fullName = fullName;
+            this.age = age;
+            this.score = score;
+        }
+
+        public string FullName => fullName;
+        public int Age => age;
+        public float Score => score;
+
+        public bool IsStudy { get;  set; }
+
+        public override string ToString()
+        {
+            return $"{fullName}; {age}; {score}; {(IsStudy ? "Studying" : "Not Studying")}";
+        }
+
+    }
+}
